3f420900f4ad7717444741e22cd52ced58cf8340
Juan Peñuelas <juanpenuelas@gmail.com>
2026-06-30 15:22:38 +0200

    chore(functions): wire callable exports + secret bindings

--- firebase/functions/index.js
+++ firebase/functions/index.js
@@ -1,3 +1,23 @@
-const functions = require("firebase-functions");
-const admin = require("firebase-admin");
-admin.initializeApp();
+const { onCall } = require("firebase-functions/v2/https");
+const { setGlobalOptions } = require("firebase-functions/v2/options");
+
+const { generateTaleDraftHandler } = require("./src/generateTaleDraft");
+const { approveDraftHandler } = require("./src/approveDraft");
+const { rejectDraftHandler } = require("./src/rejectDraft");
+
+setGlobalOptions({ maxInstances: 10 });
+
+exports.generateTaleDraft = onCall(
+  { timeoutSeconds: 120, memory: "1GiB", region: "europe-west1" },
+  generateTaleDraftHandler
+);
+
+exports.approveDraft = onCall(
+  { timeoutSeconds: 60, memory: "512MiB", region: "europe-west1" },
+  approveDraftHandler
+);
+
+exports.rejectDraft = onCall(
+  { timeoutSeconds: 60, memory: "512MiB", region: "europe-west1" },
+  rejectDraftHandler
+);
