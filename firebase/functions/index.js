8fa3dce76008c8388c61c8090b0575f179216754
Juan Peñuelas <juanpenuelas@gmail.com>
2026-06-30 16:35:06 +0200

    chore(functions): bind OPENROUTER_API_KEY + ADMIN_UID secrets to callables

--- firebase/functions/index.js
+++ firebase/functions/index.js
@@ -7,17 +7,19 @@ const { rejectDraftHandler } = require("./src/rejectDraft");
 
 setGlobalOptions({ maxInstances: 10 });
 
+const SECRETS = ["OPENROUTER_API_KEY", "ADMIN_UID"];
+
 exports.generateTaleDraft = onCall(
-  { timeoutSeconds: 120, memory: "1GiB", region: "europe-west1" },
+  { timeoutSeconds: 120, memory: "1GiB", region: "europe-west1", secrets: SECRETS },
   generateTaleDraftHandler
 );
 
 exports.approveDraft = onCall(
-  { timeoutSeconds: 60, memory: "512MiB", region: "europe-west1" },
+  { timeoutSeconds: 60, memory: "512MiB", region: "europe-west1", secrets: SECRETS },
   approveDraftHandler
 );
 
 exports.rejectDraft = onCall(
-  { timeoutSeconds: 60, memory: "512MiB", region: "europe-west1" },
+  { timeoutSeconds: 60, memory: "512MiB", region: "europe-west1", secrets: SECRETS },
   rejectDraftHandler
 );
