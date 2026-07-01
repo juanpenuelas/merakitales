7ded16489bb821a2110644816a3b83f7ba0f86d8
Juan Peñuelas <juanpenuelas@gmail.com>
2026-07-01 09:04:41 +0200

    feat(functions): wire 3-step pipeline callables + retractTale; remove generateTaleDraft

--- firebase/functions/index.js
+++ firebase/functions/index.js
@@ -1,17 +1,30 @@
 const { onCall } = require("firebase-functions/v2/https");
 const { setGlobalOptions } = require("firebase-functions/v2/options");
 
-const { generateTaleDraftHandler } = require("./src/generateTaleDraft");
+const { generateTaleTextHandler } = require("./src/generateTaleText");
+const { generateTaleImageHandler } = require("./src/generateTaleImage");
+const { generateTaleAudioHandler } = require("./src/generateTaleAudio");
 const { approveDraftHandler } = require("./src/approveDraft");
 const { rejectDraftHandler } = require("./src/rejectDraft");
+const { retractTaleHandler } = require("./src/retractTale");
 
 setGlobalOptions({ maxInstances: 10 });
 
 const SECRETS = ["OPENROUTER_API_KEY", "ADMIN_UID"];
 
-exports.generateTaleDraft = onCall(
-  { timeoutSeconds: 300, memory: "1GiB", region: "europe-west1", secrets: SECRETS },
-  generateTaleDraftHandler
+exports.generateTaleText = onCall(
+  { timeoutSeconds: 60, memory: "512MiB", region: "europe-west1", secrets: SECRETS },
+  generateTaleTextHandler
+);
+
+exports.generateTaleImage = onCall(
+  { timeoutSeconds: 120, memory: "1GiB", region: "europe-west1", secrets: SECRETS },
+  generateTaleImageHandler
+);
+
+exports.generateTaleAudio = onCall(
+  { timeoutSeconds: 60, memory: "512MiB", region: "europe-west1", secrets: SECRETS },
+  generateTaleAudioHandler
 );
 
 exports.approveDraft = onCall(
@@ -22,4 +35,9 @@ exports.approveDraft = onCall(
 exports.rejectDraft = onCall(
   { timeoutSeconds: 60, memory: "512MiB", region: "europe-west1", secrets: SECRETS },
   rejectDraftHandler
+);
+
+exports.retractTale = onCall(
+  { timeoutSeconds: 60, memory: "512MiB", region: "europe-west1", secrets: SECRETS },
+  retractTaleHandler
 );
