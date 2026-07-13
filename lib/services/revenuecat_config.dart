a03200af11144c115e3df5c32863e184b67c80cc
Juan Peñuelas <juanpenuelas@gmail.com>
2026-07-13 16:04:32 +0200

    feat(billing): add purchases_flutter dependency and configuration

---
+++ lib/services/revenuecat_config.dart
@@ -0,0 +1,5 @@
+class RevenueCatConfig {
+  static const String apiKeyAndroid = 'goog_placeholder_api_key';
+  static const String apiKeyIOS = 'appl_placeholder_api_key';
+  static const String entitlementId = 'premium';
+}
